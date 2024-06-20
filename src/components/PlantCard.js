aa0d496a425dfe42d6a74af02c0e75ff9e7e0d76
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-06-20 15:00:17 -0400

    Add ability to mark plants as sold out

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function PlantCard({ plantInfo: { name, image, price } }) {
+  const [inStock, setInStock] = useState(true)
+  const handleChangeStock = () => setInStock(!inStock)
+
   return (
     <li className='card'>
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? <button className='primary'>In Stock</button> : <button>Out of Stock</button>}
+      {inStock ? (
+        <button className='primary' onClick={handleChangeStock}>
+          In Stock
+        </button>
+      ) : (
+        <button onClick={handleChangeStock}>Out of Stock</button>
+      )}
     </li>
   )
 }
