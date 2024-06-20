2585f11b401ac4618bb4b36af6755609a00d463c
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-06-20 14:44:02 -0400

    Display all plants on app load

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,18 +1,14 @@
-import React from "react";
+import React from 'react'
 
-function PlantCard() {
+function PlantCard({ plantInfo: { name, image, price } }) {
   return (
-    <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+    <li className='card'>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      {true ? <button className='primary'>In Stock</button> : <button>Out of Stock</button>}
     </li>
-  );
+  )
 }
 
-export default PlantCard;
+export default PlantCard
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,31 @@
-import React from "react";
-import PlantCard from "./PlantCard";
+import React, { useEffect, useState } from 'react'
+import PlantCard from './PlantCard'
 
 function PlantList() {
+  const [plantList, setPlantList] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:6001/plants')
+        const data = await response.json()
+        setPlantList(data)
+      } catch (error) {
+        alert(error)
+      }
+    }
+    fetchData()
+  }, [])
+
+  if (!plantList) return <h2>Loading...</h2>
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
-  );
+    <ul className='cards'>
+      {plantList.map((plant) => {
+        return <PlantCard key={`plant-${plant.name}`} plantInfo={plant} />
+      })}
+    </ul>
+  )
 }
 
-export default PlantList;
+export default PlantList
