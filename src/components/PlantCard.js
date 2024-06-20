f0a4818bca20825938a4e3c4a2bf0def67eaed81
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-06-20 15:39:22 -0400

    Add ability to update plant prices

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -2,7 +2,10 @@ import React, { useState } from 'react'
 
 function PlantCard({ plantInfo: { id, name, image, price }, handleSubmitTrigger }) {
   const [inStock, setInStock] = useState(true)
+  const [editInput, setEditInput] = useState(false)
+  const [newPrice, setNewPrice] = useState('')
   const handleChangeStock = () => setInStock(!inStock)
+  const handleAddInput = () => setEditInput(!editInput)
 
   const handleDeletePlant = async () => {
     try {
@@ -22,10 +25,50 @@ function PlantCard({ plantInfo: { id, name, image, price }, handleSubmitTrigger 
     }
   }
 
+  const handleUpdatePrice = async () => {
+    try {
+      const response = await fetch(`http://localhost:6001/plants/${id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ price: newPrice }),
+      })
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+
+      handleSubmitTrigger()
+      setNewPrice('')
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  const EditPriceInput = () => {
+    return (
+      <>
+        <input
+          type='number'
+          value={newPrice}
+          onChange={(e) => setNewPrice(parseFloat(e.target.value))}
+          name='price'
+          step='0.01'
+          placeholder='New Price'
+        />
+        <button onClick={handleUpdatePrice}>Update Price</button>
+      </>
+    )
+  }
+
   return (
     <li className='card'>
       <img src={image} alt={name} />
       <h4>{name}</h4>
+      <span onClick={handleAddInput} style={{ cursor: 'pointer' }}>
+        ✏️
+      </span>
+      {editInput && EditPriceInput()}
       <p>Price: {price}</p>
       {inStock ? (
         <button className='primary' onClick={handleChangeStock}>
